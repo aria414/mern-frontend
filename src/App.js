ac99ef8524b216a8b31156a77ef11ee093560c11
Sean Daly <daly.se91@gmail.com>
2021-02-04 14:45:40 -0600

    Added index get route, and started create route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import "./App.css";
+// REACT IMPORTS--------------------------------------
+import React, {useState, useEffect} from 'react'
+// AXIOS IMPORTIMPORTS--------------------------------------
+import axios from 'axios'
+// COMPONENT IMPORTS ---------------------------------
 import Titlebar from "./components/TopBar";
 import Playlist from "./components/Playlist";
 import FavSong from "./components/FavoriteSongs";
 import NewSongs from "./components/NewSongs";
 function App() {
+  // define backend url base------------------------------------
+  const url = "https://mern1207-group.herokuapp.com"
+  // Defines STATES------------------------------------
+  const [playlist, setPlaylist] = useState([])
+
+  // CRUD ROUTES------------------------------------
+  // GET - INDEX---
+  const getSongs = () => {
+    axios.get(url + "/songs").then((res) => {
+      const songs = res.data;
+      setPlaylist(songs);
+    });
+  };
+  useEffect(() => {
+    getSongs();
+  }, []);
+  
+  // CREATE - ADD A SONG VIA FORM----
+  const handleCreateSong = (newSong) => {
+    axios.post(url +'/songs', newSong)
+    .then((res) => {
+      setPlaylist(...playlist, newSong);
+    })
+    .then(() => getSongs());
+  };
+  
   return (
     <div className="App">
       <Titlebar />
-      <Playlist />
+      <Playlist 
+        playlist={playlist} //Pass Down playlist state into playlist comp
+      />
       <FavSong />
       <NewSongs />
     </div>
